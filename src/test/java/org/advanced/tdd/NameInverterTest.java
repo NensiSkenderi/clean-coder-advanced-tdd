603147af52778a5498d23b190a8a06e614a05d6a
Nensi Skenderi <nensiskenderi20@gmail.com>
2022-12-12 14:56:56 +0100

    Added ignoreHonorifics test

--- src/test/java/org/advanced/tdd/NameInverterTest.java
+++ src/test/java/org/advanced/tdd/NameInverterTest.java
@@ -1,35 +1,48 @@
 package org.advanced.tdd;
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class NameInverterTest {
 
     @Test
-    public void givenNull_returnsEmptyString(){
-        assertInverted("", null);
+    public void givenNull_returnsEmptyString() {
+        assertInverted(null, "");
     }
 
     @Test
-    public void givenEmptyString_returnsEmptyString(){
+    public void givenEmptyString_returnsEmptyString() {
         assertInverted("", "");
     }
 
     @Test
-    public void givenSimpleName_returnSimpleName(){
+    public void givenSimpleName_returnSimpleName() {
         assertInverted("Nensi", "Nensi");
     }
 
     @Test
-    public void givenFirstLast_returnLastFirst(){
+    public void givenFirstLast_returnLastFirst() {
         assertInverted("First Last", "Last First");
     }
-    public void givenASimpleNameWithSpaces_returnSimpleNameWithoutSpaces(){
+
+    public void givenASimpleNameWithSpaces_returnSimpleNameWithoutSpaces() {
         assertInverted(" Name ", "Name");
     }
+
     @Test
-    public void givenFirstLastWithExtraSpaces_returnLastFirst(){
+    public void givenFirstLastWithExtraSpaces_returnLastFirst() {
         assertInverted(" First     Last    ", "Last First");
+    }
+
+    @Test
+    public void ignoreHonorifics() {
+        assertInverted("Mr. First Last", "Last First");
+        assertInverted("Mrs. First Last", "Last First");
     }
 
     private void assertInverted(String originalName, String invertedName) {
@@ -37,16 +50,24 @@ public class NameInverterTest {
     }
 
     private String invertName(String name) {
-        if (name == null || name.length() <= 0) {
+        if (name == null || name.length() == 0) {
             return "";
+        } else {
+            List<String> names = splitNames(name);
+            if (names.size() > 1 && isHonorific(names.get(0)))
+                names.remove(0);
+            if (names.size() == 1)
+                return names.get(0);
+            else
+                return String.format("%s %s", names.get(1), names.get(0));
         }
-        else {
-            name = name.trim();
-            String[] names = name.split("\\s+"); //regex to check for extra spaces
-            if(names.length == 1){
-                return name;
-            }
-            return String.format("%s %s", names[1], names[0]);
-        }
+    }
+
+    private static boolean isHonorific(String word) {
+        return word.matches("Mr\\.|Mrs\\.");
+    }
+
+    private static ArrayList<String> splitNames(String name) {
+        return new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
     }
 }
